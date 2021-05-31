350d10f9a793a05a06d5d152754f95b640b550f4
Tommy <tommyho@cpp.edu>
2021-05-31 14:24:12 -0700

    ran prettier on works.js

--- works.js
+++ works.js
@@ -3,33 +3,33 @@ let workIndex = 1;
 showWork(workIndex);
 
 function nextWork(n) {
-    showWork(workIndex += n)
+  showWork((workIndex += n));
 }
 
 function currentWork(n) {
-    showWork(workIndex = n);
+  showWork((workIndex = n));
 }
 
 function showWork(n) {
-    let works = document.getElementsByClassName("works");
-    let index = document.getElementsByClassName("index");
+  let works = document.getElementsByClassName("works");
+  let index = document.getElementsByClassName("index");
 
-    if (n > works.length) {
-        workIndex = 1;
-    }
+  if (n > works.length) {
+    workIndex = 1;
+  }
 
-    if (n < 1) {
-        workIndex = works.length;
-    }
+  if (n < 1) {
+    workIndex = works.length;
+  }
 
-    for (let i = 0; i < works.length; i++) {
-        works[i].style.display = "none";
-    }
+  for (let i = 0; i < works.length; i++) {
+    works[i].style.display = "none";
+  }
 
-    for(let i = 0; i < index.length; i++) {
-        index[i].className = index[i].className.replace(" active", "");
-    }
-    
-    works[workIndex-1].style.display = "block";
-    index[workIndex-1].className += " active";
+  for (let i = 0; i < index.length; i++) {
+    index[i].className = index[i].className.replace(" active", "");
+  }
+
+  works[workIndex - 1].style.display = "block";
+  index[workIndex - 1].className += " active";
 }
